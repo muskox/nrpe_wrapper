1ba50191ed8012598d3b9aba021c9b384ad5aba3
Scott Pustay <spustay@safaribooksonline.com>
2014-07-17 13:14:26 -0700

    adding metadata

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'nrpe_wrapper'
+maintainer       'Scott Pustay'
+maintainer_email 'scott.pustay@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures nrpe_wrapper'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends		 'nrpe'
